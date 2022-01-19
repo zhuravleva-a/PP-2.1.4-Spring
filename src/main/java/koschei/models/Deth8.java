88e2954307cd56b2079852f9c123b096a7f83fe1
Anastasia Zhuravleva <agzhuravleva@gmail.com>
2022-01-19 18:03:13 +0300

    Initial solution

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    @Autowired
+    public Deth8() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
